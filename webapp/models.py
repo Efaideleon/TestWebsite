035ae8dc5a7d1699a706c1b209e726f1af5d0d88
Efaideleon <eedeleon@uci.edu>
2023-03-27 13:33:02 -0700

    aaa

--- webapp/models.py
+++ webapp/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=100, default='', null=True)
-    def __str__(self):
-        return self.name
-
 class dataMCU(models.Model):
+    user =models.ManyToOneRel(User, on_delete=models.CASCADE, null=True)
     soil_moisture = models.IntegerField(default=0)
     soil_temp = models.FloatField(default=0)
     wind_speed = models.FloatField(default=0)
